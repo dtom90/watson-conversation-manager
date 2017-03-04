9c7e33910e152362f9632ead78e785ef3b317a6e
David Thomason <dlthomas@us.ibm.com>
2017-03-03 19:00:53 -0500

    Added utterance test form

--- workspace_view.py
+++ workspace_view.py
@@ -1,13 +1,25 @@
-from flask import render_template
+from flask import render_template, request
 from flask.views import View
+from wtforms import Form, StringField
 import conversation
 
 
 class WorkspaceView(View):
     def dispatch_request(self, workspace_id):
         workspace = conversation.workspace_info(workspace_id)
+
         max_examples = 0
         for intent in workspace['intents']:
             max_examples = max(max_examples, len(intent['examples']))
-        print(max_examples)
-        return render_template('workspace.html', workspace=workspace, max_examples=max_examples)
+
+        test_form = TestForm()
+
+        utterance = request.args.get('utterance')
+        test_response = conversation.message(workspace_id, utterance)['intents'] if utterance else None
+
+        return render_template('workspace.html', workspace=workspace, max_examples=max_examples,
+                                                 form=test_form, utterance=utterance, intents=test_response)
+
+
+class TestForm(Form):
+    utterance = StringField('Utterance')
