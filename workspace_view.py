7d5e33696e49ea3aaf802e660334f68492f7a22b
David Thomason <dlthomas@us.ibm.com>
2017-03-24 15:44:11 -0400

    Condensed repeats hash construction

--- workspace_view.py
+++ workspace_view.py
@@ -11,8 +11,10 @@ class WorkspaceView(View):
         if 'err' in workspace:
             return workspace['err']
 
-        max_examples = 0
+        repeat_examples = []
         all_examples = {}
+        max_examples = 0
+        max_repeats = 0
         for intent in workspace['intents']:
             name = intent['intent']
             examples = intent['examples']
@@ -21,15 +23,13 @@ class WorkspaceView(View):
                 text = example['text'].lower()
                 if text in all_examples:
                     all_examples[text].append(name)
+                    count = len(all_examples[text])
+                    if count == 2: repeat_examples.append(text)
+                    max_repeats = max(max_repeats, count)
                 else:
                     all_examples[text] = [name]
 
-        repeats = {}
-        max_repeats = 0
-        for example in all_examples:
-            if len(all_examples[example]) > 1:
-                repeats[example] = all_examples[example]
-                max_repeats = max(max_repeats, len(repeats[example]))
+        repeats = {ex: all_examples[ex] for ex in repeat_examples}
 
         test_form = TestForm()
 
@@ -40,6 +40,7 @@ class WorkspaceView(View):
                                                  repeats=repeats, max_repeats=max_repeats,
                                                  form=test_form, message=message,
                                                  message_output=message_output, )
+
 
 class TestForm(Form):
     message = StringField('Message')
